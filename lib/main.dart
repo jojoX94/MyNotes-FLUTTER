e3573bbd781ff9d736d8ac1a65f344d48230f5b8
jojoX94 <ramarolacontact@gmail.com>
2022-12-15 16:34:31 +0300

    Create NewRouteView screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/notes_view.dart';
+import 'package:mynotes/views/notes/new_note_view.dart';
+import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 
@@ -20,6 +21,7 @@ void main() async {
       registerRoute: (context) => const RegisterView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
       notesRoute: (context) => const NotesView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
